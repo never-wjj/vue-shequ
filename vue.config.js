28307ff71ed174c878d285909e336951a74c8b39
unknown <never-wjj>
2019-07-15 15:56:09 +0800

    she

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-shequ/" : "/",
   css: {
     loaderOptions: {
       stylus: {
-        'resolve url': true,
-        'import': []
+        "resolve url": true,
+        import: []
       }
     }
   },
   pluginOptions: {
-    'cube-ui': {
+    "cube-ui": {
       postCompile: true,
       theme: false
     }
   }
-}
+};
